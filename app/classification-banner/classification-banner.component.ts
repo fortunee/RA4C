78b565cfd3e740261c9d470a6baa98336cbf1b93
Fortune Ekeruo <legalmody@gmail.com>
2017-07-28 12:32:51 +0100

    Add styleUrls component property

--- app/classification-banner/classification-banner.component.ts
+++ app/classification-banner/classification-banner.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nb-classification-banner',
-    templateUrl: 'app/classification-banner/classification-banner.template.html'
+    templateUrl: 'app/classification-banner/classification-banner.template.html',
+    styleUrls: ['classification-banner.component.css']
 })
 export class ClassificationBannerComponent {
     public pageTitle: string = 'Classifiction Banner Component';

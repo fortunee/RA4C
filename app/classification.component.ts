20979cc9fe0192eaecf8bc2c9d773ed0144df3a1
Fortune Ekeruo <legalmody@gmail.com>
2017-07-28 15:46:59 +0100

    Nesting classification banner component with an Input decorator

--- app/classification.component.ts
+++ app/classification.component.ts
@@ -3,9 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'classification',
   template: `
-  <div>
-    <nb-classification-banner></nb-classification-banner>
-  </div>
+    <div>
+      <nb-classification-banner 
+        acmObject="an object coming through"
+        canSetLevel="true"
+        environment="testing"
+        ></nb-classification-banner>
+    </div>
   `
 })
-export class ClassificationComponent {}
+export class ClassificationComponent {
+  
+}

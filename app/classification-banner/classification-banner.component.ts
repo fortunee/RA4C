247f0dceab81ae09d26e6efa2f9c19549b7a7afd
Fortune Ekeruo <legalmody@gmail.com>
2017-07-29 18:09:40 +0100

    Change background color when a property of acmObject changes

--- app/classification-banner/classification-banner.component.ts
+++ app/classification-banner/classification-banner.component.ts
@@ -1,28 +1,32 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'nb-classification-banner',
     templateUrl: 'app/classification-banner/classification-banner.template.html',
     styleUrls: ['app/classification-banner/classification-banner.component.css']
 })
-export class ClassificationBannerComponent {
+export class ClassificationBannerComponent implements OnChanges {
 
     public pageTitle: string = 'Classifiction Banner Component';
     @Input() acmObject: any;
     @Input() canSetLevel: boolean;
     @Input() environment: string = 'dev'; // Set dev by default
     @Input() position: string;
+    backgroundColor: string = 'black';
 
     constructor() {
         
     }
 
-    onClick(): void {
-        // Something needs to happen
-        console.log(this.acmObject);
-        console.log(this.canSetLevel);
-        console.log(this.environment);
+    ngOnChanges(): void {
+        if(this.acmObject.classif === 'U') {
+            this.backgroundColor = 'green';
+        } else if (this.acmObject.classif === 'TS') {
+            this.backgroundColor = 'yellow'
+        }
     }
 
-    
+    onClick(): void {
+        // Something needs to happen
+    }
 }
